59d122b92ca4e2da416193bb436a355560a881da
thanosch94 <chatzopoulosthanos@gmail.com>
2026-05-15 00:31:46 +0300

    Test change

---
+++ Data/Enums/TrafficSourceEnum.cs
@@ -0,0 +1,16 @@
+﻿namespace StoreApi.Data.Enums
+{
+    public enum TrafficSourceEnum
+    {
+        Direct = 0,
+        Google = 1,
+        Bing = 2,
+        Facebook = 3,
+        Instagram = 4,
+        Youtube = 5,
+        Tiktok = 6,
+        Referral = 7,
+
+        Internal = 8,
+    }
+}
