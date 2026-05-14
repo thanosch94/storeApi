89eba71b22ea8d7088b53ff896fa3f876f5bafdb
thanosch94 <chatzopoulosthanos@gmail.com>
2026-05-15 00:27:03 +0300

    Dto Changes

---
+++ Data/Dto/CategoryDto.cs
@@ -0,0 +1,14 @@
+﻿namespace StoreApi.Data.Dto
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public Guid? ParentId { get; set; }
+        public required string Name { get; set; }
+        public required string Slug { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Description { get; set; }
+        public string? SeoTitle { get; set; }
+        public string? MetaDescription { get; set; }
+    }
+}
---
+++ Data/Dto/OptionsListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace StoreApi.Data.Dto
+{
+    public class OptionsListDto<T>
+    {
+        public List<T> List { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
